9bb732a0219d25247f1cbcf78b23833e2eae3794
Otto Borchert <borchert-o@mssu.edu>
2021-02-01 07:47:18 -0600

    Initial Commit

---
+++ SlotMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlotMachine
+//
+//  Created by Borchert, Otto on 2/1/21.
+//  Copyright © 2021 Missouri Southern State University - CIS 395. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
